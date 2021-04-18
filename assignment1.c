73a0a0160b29e1e6b3d1268237fa60f871e09914
Tam Do <dantam96@gmail.com>
2021-04-17 21:23:26 -0500

    4-17 version

--- assignment1.c
+++ assignment1.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int main() {
 	//game board
 	char board[5][3];
@@ -5,48 +6,48 @@ int main() {
         for(int i=0; i<3; i++){
 		for(int j=0; j<5;j++){
 			if(j%2==0) {
-				board[i][j] = "-";
+				board[i][j] = '-';
 			}
 			else {
-				board[i][j] = "|";
-			}
+				board[i][j] = '|';
+		}
 		}
 
 	}	
-	puts("Welcome to Jenny and Tam's Wicked TicTacToe\n");
-	puts("Input moves by: row, col. E.g.\n");
-	puts("-|-|-\n");
-	puts("-|-|-\n");
-	puts("-|-|-\n");
-	puts("Player X: Select square:\n");
-	puts("2,3");
-	puts("-|-|-\n");
-	puts("-|-|X\n");
-	puts("-|-|-\n");
-	puts("==================================\n");
-	puts("Play!\n");
+	printf("Welcome to Jenny and Tam's Wicked TicTacToe\n");
+	printf("Input moves by: row, col. E.g.\n");
+	printf("-|-|-\n");
+	printf("-|-|-\n");
+	printf("-|-|-\n");
+	printf("Player X: Select square:\n");
+	printf("2,3");
+	printf("-|-|-\n");
+	printf("-|-|X\n");
+	printf("-|-|-\n");
+	printf("==================================\n");
+	printf("Play!\n");
 
 	//game loop
 	while(1) {
 		//Prompt Play X for input
 
-		puts("Player X: Select square:\n");
-		char *input[3];
-	        gets(input);
+		printf("Player X: Select square:\n");
+		char  *input[3];
+	        scanf("%c%c%c",input);
 		
-		int userrow=input[0]-1;
-		int usercol=input[2];
+		char  userrow=*input[0]-1;
+		char  usercol=*input[2];
 		if(usercol==1){
 			usercol=0;
 		}
 		if(usercol==3){
 			usercol=4;
 		}
-		board[userrow][usercol]="X";
+		board[userrow][usercol]='X';
 		//Print State of Game Board
-		puts(board[0]);
-		puts(board[1]);
-		puts(board[2]);
+		printf(board[0]);
+		printf(board[1]);
+		printf(board[2]);
 
 	
 
